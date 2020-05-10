59bd0ddf51236e6b56d1981f3b4d8679fa067d07
gregory.clayton <gregory.clayton@thinkmoneygroup.com>
2020-05-10 13:44:38 +0100

    Removed changes

--- gulpfile.js
+++ gulpfile.js
@@ -60,14 +60,13 @@ gulp.task('browserSync', gulp.series(function () {
     },
     notify: false
   })
-  
+  done();
 }));
 
 gulp.task('watch', gulp.series(['browserSync', 'sass', 'scripts'], function () {
   gulp.watch('main/*.html', gulp.series(reload));
   gulp.watch('main/assets/css/**/*.scss', gulp.series(['sass']));
   gulp.watch(componentsJsPath, gulp.series(['scripts']));
-  done();
 }));
 
 
